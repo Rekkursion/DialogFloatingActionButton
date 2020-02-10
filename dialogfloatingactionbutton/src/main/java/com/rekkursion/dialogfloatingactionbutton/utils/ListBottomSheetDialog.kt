e51bd881f981797362df3545f3e6a37983e2d83c
Rekkursion <rekkursion233@gmail.com>
2020-02-10 14:20:21 +0800

    Fix the bug that if there is no on-item-click-listener, the independent on-click-listener will not be invoked

--- dialogfloatingactionbutton/src/main/java/com/rekkursion/dialogfloatingactionbutton/utils/ListBottomSheetDialog.kt
+++ dialogfloatingactionbutton/src/main/java/com/rekkursion/dialogfloatingactionbutton/utils/ListBottomSheetDialog.kt
@@ -40,8 +40,12 @@ internal class ListBottomSheetDialog(context: Context): BottomSheetDialog(contex
         mAdapter.insert(itemName, index)
 
         // add the designated on-click-listener to the list if has one
-        if (onClickListener == null) mOnClickListenerList.add(index, null)
-        else mOnClickListenerList.add(index, Pair(onClickListener, dismissAfterClicking))
+        if (onClickListener == null)
+            mOnClickListenerList.add(index, null)
+        else {
+            mOnClickListenerList.add(index, Pair(onClickListener, dismissAfterClicking))
+            setOnItemClickListener(null, null)
+        }
 
         // set the content view
         if (mItemListView.parent == null)
@@ -51,15 +55,15 @@ internal class ListBottomSheetDialog(context: Context): BottomSheetDialog(contex
     /* ============================================================ */
 
     // set the on-item-click-listener
-    internal fun setOnItemClickListener(onItemClickListener: AdapterView.OnItemClickListener, dismissAfterClicking: Boolean) {
+    internal fun setOnItemClickListener(onItemClickListener: AdapterView.OnItemClickListener?, dismissAfterClicking: Boolean?) {
         val listener = AdapterView.OnItemClickListener { adapterView, view, index, id ->
-            if (mOnClickListenerList[index] == null) {
-                onItemClickListener.onItemClick(adapterView, view, index, id)
-                if (dismissAfterClicking) this.dismiss()
-            }
-            else {
+            if (mOnClickListenerList[index] != null) {
                 mOnClickListenerList[index]?.first?.onClick(view)
                 if (mOnClickListenerList[index]!!.second) this.dismiss()
+            }
+            else if (onItemClickListener != null) {
+                onItemClickListener.onItemClick(adapterView, view, index, id)
+                if (dismissAfterClicking == true) this.dismiss()
             }
         }
         mItemListView.onItemClickListener = listener
